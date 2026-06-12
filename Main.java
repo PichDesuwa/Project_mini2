69358b790d98a74970e6012da71b5e7e72455058
PichDesuwa <pichbothun@gmail.com>
2026-06-12 14:40:10 +0700

    push

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         Parent root = loader.load(getClass().getResource("view.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     public static void main(String[] args) {
         launch(args);
