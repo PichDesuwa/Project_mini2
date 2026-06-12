4f95d04d27df97a9b2c22d568e93b5abefb4f226
saydara2004-source <say.dara2004@gmail.com>
2026-06-12 14:34:05 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-public class Main {
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
     
 }
