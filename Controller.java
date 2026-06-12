74197198856f21e4fff2a015b0925e63298ac956
mAiyc <lyzatouy@gmail.com>
2026-06-12 14:08:35 +0700

    wsdhj

---
+++ Controller.java
@@ -0,0 +1,29 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Button bt1;
+
+    @FXML
+    private ChoiceBox<?> cb1;
+
+    @FXML
+    private ChoiceBox<?> cb2;
+
+    @FXML
+    private ChoiceBox<?> cb3;
+
+    @FXML
+    private Label result;
+
+    @FXML
+    void on_button(ActionEvent event) {
+
+    }
+
+}
